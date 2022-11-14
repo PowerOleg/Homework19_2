60c3ca6ef6bea65094c76fb02a07e40608d654a0
PowerOleg <geraskin.oleg@mail.ru>
2022-11-14 17:10:38 +0500

    WordsChecker is done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,3 @@
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 public class Main {
     private static String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
             " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam," +
@@ -14,29 +8,8 @@ public class Main {
     public static void main(String[] args) {
         WordsChecker wordsChecker = new WordsChecker(text);
         wordsChecker.splitText();
-
-
-
-        /*
-        Iterator<Integer> iterator = text.toLowerCase().chars().iterator();
-        while (iterator.hasNext()) {
-            int i = iterator.next();
-            char ch = (char) i;
-            if (!hashMap.containsKey(ch)) hashMap.put(ch, 1);
-            else containsKey(hashMap, ch);
-        }
-        List<Map.Entry<Character, Integer>> hashMap1 =
-                hashMap.entrySet().stream().filter(n -> Character.isLetter(n.getKey())).collect(Collectors.toList());
-        hashMap1.sort((o1, o2) -> o1.getValue() - o2.getValue());
-
-        String min = String.valueOf(hashMap1.get(0));
-        String max = String.valueOf(hashMap1.get(hashMap1.size() - 1));
-        System.out.printf("The letter %s appears the most often\n", max.charAt(0));
-        System.out.printf("The letter %s is the rarest\n", min.charAt(0));
-        System.out.println();
-        System.out.println("To check yourself let's take a look at the list:");
-        hashMap1.stream().forEach(System.out::println);
-        */
-
+        System.out.println(wordsChecker.hasWord("salut"));
+        System.out.println(wordsChecker.hasWord("LABORE"));
+        System.out.println(wordsChecker.hasWord("dolore"));
     }
 }
--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -3,32 +3,29 @@ import java.util.Set;
 
 public class WordsChecker {
     String text;
-    //Set потому что слова должны не повторяться в коллекции
     Set<String> set = new HashSet<>();
 
     public WordsChecker(String text) {
         this.text = text.toLowerCase();
     }
-public void splitText() {
+    public void splitText() {
         String[] word = text.split(" ");
-    for (int i = 0; i < word.length; i++) {
-        if (word[i].charAt(word[i].length() - 1) == '.')  {
-            String[] s = word[i].split("\\.");
-            word[i] = s[0];
-        } else if (word[i].charAt(word[i].length() - 1) == ',') {
-            String[] s = word[i].split(",");
-            word[i] = s[0];
-        } else set.add(word[i]);
-        System.out.println(word[i]);
+        for (int i = 0; i < word.length; i++) {
+            if (word[i].charAt(word[i].length() - 1) == '.') {
+                String[] s = word[i].split("\\.");
+                word[i] = s[0];
+            } else if (word[i].charAt(word[i].length() - 1) == ',') {
+                String[] s = word[i].split(",");
+                word[i] = s[0];
+            } else set.add(word[i]);
+        }
     }
-
-}
-
-//    public boolean hasWord(String word) {
-//        for ( : ) {
-//            text.contains(word);
-//        }
-//        return true;
-//    }
-
+    public boolean hasWord(String word) {
+        for (String s : set){
+            if (text.contains(word.toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
